b717c0ffbb9a8285b6300cf92b84e94771b98888
codejihyekim <wlgp03090@gmail.com>
2022-03-18 17:52:50 +0900

    2022-03-18 17:52 각 컬럼 코딩

--- titanic/models.py
+++ titanic/models.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from icecream import ic
 
 from context.domains import Dataset
@@ -19,8 +21,17 @@ class TitanicModel(object):
         # Entity에서 Object로 전환
         this = self.drop_feature(this, 'Ticket', 'Cabin', 'Parch', 'SibSp')
         # self.kwargs_sample(name='이순신')
+        this = self.extract_title_from_name(this)
+        title_mapping = self.remove_duplicate(this)
+        this = self.title_nominal(this, title_mapping)
+        this = self.drop_feature(this, 'Name')
+        this = self.sex_nominal(this)
+        this = self.drop_feature(this, 'Sex')
+        this = self.embarked_nominal(this)
+        this = self.age_ratio(this)
+        #this = self.name_nominal(this)
+
         '''
-        this = self.name_nominal(this)
         this = self.pclass_ordinal(this)
         this = self.embarked_nominal(this)
         this = self.fare_ratio(this)
@@ -30,24 +41,23 @@ class TitanicModel(object):
         this = self.create_train(this)
         '''
 
-        self.print_this(this)
+        self.df_info(this)
         return this
 
     @staticmethod
-    def print_this(this):
-        print('*'*100)
-        ic(f'1. Train 의 타입 :  {type(this.train)}\n')
-        ic(f'2. Train 의 컬럼 : {this.train.columns}\n')
-        ic(f'3. Train 의 상위 1개 : {this.train.head(1)}\n')
-        ic(f'4. Train 의 null의 개수 : {this.train.isnull().sum()}\n')
-        ic(f'5. Test 의 타입 : {type(this.train)}\n')
-        ic(f'6. Test 의 컬럼 : {this.train.columns}\n')
-        ic(f'7. Test 의 상위 1개 : {this.train.head(1)}\n')
-        ic(f'8. Test 의 null의 개수 : {this.train.isnull().sum()}\n')
-        ic(f'9. id의 타입 : {type(this.id)}\n')
-        ic(f'10. id의 상위 10개 : {this.id[:10]}\n')
-        print('*' * 100)
+    def df_info(this):
+        [ic(f'{i.info()}') for i in [this.train, this.test]]
+        ic(this.train.head(3))
+        ic(this.test.head(3))
 
+    @staticmethod
+    def null_check(this):
+        [ic(f'{i.isnull().sum()}') for i in [this.train, this.test]]
+
+    @staticmethod
+    def id_info(this):
+        ic(f'9. id 의 타입  {type(this.id)}')
+        ic(f'10. id 의 상위 3개 {this.id[:3]}')
 
     @staticmethod
     def drop_feature(this, *feature) -> object:
@@ -76,8 +86,45 @@ class TitanicModel(object):
     def pclass_ordinal(this) -> object:
         return this
 
+
     @staticmethod
-    def name_nominal(this) -> object:
+    def extract_title_from_name(this) -> object:
+        combine = [this.train, this.test]  # 데이터프레임을 다시 분해
+        for dataset in combine:
+            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        ic(this.train.head(5))
+        return this
+
+    @staticmethod
+    def remove_duplicate(this) -> None:
+        a = []
+        for dataset in [this.train, this.test]:
+            a += list(set(dataset['Title']))
+        a = list(set(a))
+        #print((f'>>>> {a}'))
+        '''
+        ['Mr', 'Sir', 'Major', 'Don', 'Rev', 'Countess', 'Lady', 'Jonkheer', 'Dr',
+        'Miss', 'Col', 'Ms', 'Dona', 'Mlle', 'Mme', 'Mrs', 'Master', 'Capt']
+        Royal : ['Countess', 'Lady', 'Sir']
+        Rare : ['Capt','Col','Don','Dr','Major','Rev','Jonkheer','Dona','Mme']
+        Mr : ['Mlle']
+        Ms : ['Miss']
+        Master
+        Mrs
+        '''
+        title_mapping = {'Mr': 1, 'Ms': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
+        return title_mapping
+
+    @staticmethod
+    def title_nominal(this, title_mapping) -> object:
+        combine = [this.train, this.test]  # 데이터프레임을 다시 분해
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            dataset['Title'] = dataset['Title'].replace(['Capt','Col','Don','Dr','Major','Rev','Jonkheer','Dona','Mme' ], 'Rare')
+            dataset['Title'] = dataset['Title'].replace(['Mlle'], 'Mr')
+            dataset['Title'] = dataset['Title'].replace(['Miss'], 'Ms')
+            dataset['Title'] = dataset['Title'].fillna(0)
+            dataset['Title'] = dataset['Title'].map(title_mapping)
         return this
 
     @staticmethod
@@ -86,12 +133,31 @@ class TitanicModel(object):
 
     @staticmethod
     def sex_nominal(this) -> object:
+        gender_mapping = {'male': 0, 'female': 1}
+        for these in [this.train, this.test]:
+            these['Gender'] = these['Sex'].map(gender_mapping)
         return this
 
     @staticmethod
     def age_ratio(this) -> object:
+        train = this.train
+        test = this.test
+        age_mapping = {'Unknown': 0, 'Baby': 1, 'Child': 2, 'Teenager': 3, 'Student': 4,
+                       'Young Adult': 5, 'Adult': 6, 'Senior': 7}
+        train['Age'] = train['Age'].fillna(-0.5)
+        test['Age'] = test['Age'].fillna(-0.5)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+        for these in train, test:
+            # pd.cut() 을 사용하시오. 다른 곳은 고치지 말고 다음 두 줄만 코딩하시오
+            these['AgeGroup'] = pd.cut(these['Age'], bins, labels=labels)  # pd.cut() 을 사용
+            these['AgeGroup'] = these['AgeGroup'].map(age_mapping)  # map() 을 사용
         return this
 
     @staticmethod
     def embarked_nominal(this) -> object:
+        embarked_mapping = {'S': 1, 'C': 2, 'Q': 3}
+        this.train = this.train.fillna({'Embarked': 'S'})
+        for these in [this.train, this.test]:
+            these['Embarked'] = these['Embarked'].map(embarked_mapping)
         return this

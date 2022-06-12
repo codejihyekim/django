d444ec0de00fa1f3bf91e90b1d0c21a8d5998bae
codejihyekim <wlgp03090@gmail.com>
2022-06-12 22:03:49 +0900

    2022-06-12 crime 복습

---
+++ crime/crime.py
@@ -0,0 +1,10 @@
+from context.domains import Reader, File
+import folium
+
+class Solution(Reader):
+
+    def __init__(self):
+        self.file = File()
+        self.crime_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']
+        self.crime_columns = ['살인', '강도', '강간', '절도', '폭력']
+        self.file.context = './data/'

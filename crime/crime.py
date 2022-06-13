442181308eabbd341feeb67e706b6f87a816e3a7
codejihyekim <wlgp03090@gmail.com>
2022-06-13 23:14:24 +0900

    2022-06-12 hook

--- crime/crime.py
+++ crime/crime.py
@@ -8,3 +8,25 @@ class Solution(Reader):
         self.crime_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']
         self.crime_columns = ['살인', '강도', '강간', '절도', '폭력']
         self.file.context = './data/'
+
+    def hook(self):
+        def print_menu():
+            print('0. Exit')
+            print('1. crime_in_seoul.csv, 구글맵 API 를 이용해서 서울시내 경찰서 주소목록파일(police_pos.csv)을 작성하시오.')
+            print('2. us-states.json, us_unemployment.csv 를 이용해서 미국 실업률 지도(folium_test.html)를 작성하시오.')
+            print('3. cctv_in_seoul.csv, pop_in_seoul.csv 를 이용해서 서울시내 경찰서 주소목록파일(cctv_pop.csv)을 작성하시오.')
+            print('4. police_pos.csv, 를 이용해서 경찰서 범죄검거율 정규화파일(police_norm.csv)을 작성하시오.')
+            return input('메뉴 선택 \n')
+
+        while 1:
+            menu = print_menu()
+            if menu == '0':
+                break
+            if menu == '1':
+                pass
+            if menu == '2':
+                pass
+            if menu == '3':
+                pass
+            elif menu == '4':
+                pass
